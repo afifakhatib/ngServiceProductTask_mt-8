f5c98fc668312bde2c84dac95f90dc52ca76e862
afifakhatib <khatibadiba12@gmail.com>
2024-07-11 10:31:45 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngServiceProductTask_mt-8';
+}
